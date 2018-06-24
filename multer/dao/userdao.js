110a6920365952ccefa8847f8cb169b723476336
ytdxming <ytdxming@126.com>
2018-06-24 22:19:25 +0800

    commit in shenzhen on vs

--- multer/dao/userdao.js
+++ multer/dao/userdao.js
@@ -1,17 +1,17 @@
 
 
-var db=require('../common/mysqlbasicconn');
+var db = require('../common/mysqlbasicconn');
 
 
 //var sql='';
 
-var jsonWrite=function(res,ret){
-	if(typeof ret=='undefined'){
+var jsonWrite = function (res, ret) {
+	if (typeof ret == 'undefined') {
 		res.json({
-			code:'1',
-			msg:'Can not do it'
+			code: '1',
+			msg: 'Can not do it'
 		});
-	}else {
+	} else {
 		res.json(ret)
 	}
 };
@@ -19,123 +19,123 @@ var jsonWrite=function(res,ret){
 
 
 
-module.exports={
-	
-	listuser: function(req,res,next){ 
-		db.query('select * from user',(err,result)=>{
-		if(err){
-			console.log(err);
-			return;
-		};
-		console.log('*********display all user***********');
-		//res.render('index.ejs',result:result);
-		console.log(result);
+module.exports = {
 
-		res.render('userlist.ejs',{userList:result});
-		
+	listuser: function (req, res, next) {
+		db.query('select * from user', (err, result) => {
+			if (err) {
+				console.log(err);
+				return;
+			};
+			console.log('*********display all user***********');
+			//res.render('index.ejs',result:result);
+			console.log(result);
+
+			res.render('userlist.ejs', { userList: result });
+
 		});
 	},
 
 
 
-	insertuser: function(req,res,next){
-		  //res.render('adduser.ejs');
-	var param=req.query||req.params;
-	/**
-	*/
+	insertuser: function (req, res, next) {
+		//res.render('adduser.ejs');
+		var param = req.query || req.params;
+		/**
+		*/
 
-	console.log("***Raram is not null ");
-	console.log(param);
-	//console.log("*****"+param.id,param.name,param.age);
+		console.log("***Raram is not null ");
+		console.log(param);
+		//console.log("*****"+param.id,param.name,param.age);
 
-	if(param.name==null || param.age==null) {
-		jsonWrite(res,undefined);
-		return;
-	}
-
-
-	console.log("***Raram is not null ");
-	console.log("*****"+param.id,param.name,param.age);
-
-
-
-
-
-	db.query('INSERT INTO user (id,name,age) VALUES(?,?,?)',[param.id,param.name,param.age],(err,result)=>{
-		if(err){
-			console.log(err);
+		if (param.name == null || param.age == null) {
+			jsonWrite(res, undefined);
 			return;
-		};
-		console.log(result.affectedRows);
-
-		if(result){
-			res.render('success');
-//console.log("success");
-			
-
-		//res.render('userlist.ejs',{userList:result});
-		}else {
-			res.render('error');
-		
 		}
-		console.log('*********addoneuser user***********');
-		//res.render('index.ejs',result:result);
-		console.log(result);
-		return;
 
-		//res.render('userlist.ejs',{userList:result});
-		
-	});	
+
+		console.log("***Raram is not null ");
+		console.log("*****" + param.id, param.name, param.age);
+
+
+
+
+
+		db.query('INSERT INTO user (id,name,age) VALUES(?,?,?)', [param.id, param.name, param.age], (err, result) => {
+			if (err) {
+				console.log(err);
+				return;
+			};
+			console.log(result.affectedRows);
+
+			if (result) {
+				res.render('success');
+				//console.log("success");
+
+
+				//res.render('userlist.ejs',{userList:result});
+			} else {
+				res.render('error');
+
+			}
+			console.log('*********addoneuser user***********');
+			//res.render('index.ejs',result:result);
+			console.log(result);
+			return;
+
+			//res.render('userlist.ejs',{userList:result});
+
+		});
 
 	},
 
 
 
-	delete:function(req,res,next){
-			  //res.render('adduser.ejs');
-	var param=req.query||req.params;
-	/**
-	*/
+	delete: function (req, res, next) {
+		//res.render('adduser.ejs');
+		var param = req.query || req.params;
+		/**
+		*/
 
-	console.log("***Raram is not null ");
-	console.log(param);
-	//console.log("*****"+param.id,param.name,param.age);
+		console.log("***Raram is not null ");
+		console.log(param);
+		//console.log("*****"+param.id,param.name,param.age);
 
-	if(param.id==null ) {
-		jsonWrite(res,undefined);
-		return;
-	}
-
-
-	console.log("***Raram is not null ");
-	console.log("*****"+param.id,param.name,param.age);
-
-
-
-	db.query('delete  from user where id=?',[param.id],(err,result)=>{
-if(err){
-	console.log(err);
-	return;
-};
-
-
-
-if(result){
-			res.render('success');
-//console.log("success");
-			
-
-		//res.render('userlist.ejs',{userList:result});
-		}else {
-			res.render('error');
-		
+		if (param.id == null) {
+			jsonWrite(res, undefined);
+			return;
 		}
-		console.log('*********delete user***********');
-		//res.render('index.ejs',result:result);
-		console.log(result);
-		return;
 
-	})
+
+		console.log("***Raram is not null ");
+		console.log("*****" + param.id, param.name, param.age);
+
+
+
+		db.query('delete  from user where id=?', [param.id], (err, result) => {
+			if (err) {
+				console.log(err);
+				return;
+			};
+
+
+
+			if (result) {
+				res.render('success');
+				//console.log("success");
+
+
+				//res.render('userlist.ejs',{userList:result});
+			} else {
+				res.render('error');
+
+			}
+			console.log('*********delete user***********');
+			//res.render('index.ejs',result:result);
+			console.log(result);
+			return;
+
+		})
 
 	}
 
